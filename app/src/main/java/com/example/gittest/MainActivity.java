5e58d4ba6a7f278e1a31414ef3e7e83aea83477a
Huangtianhao <276354355@qq.com>
2019-11-28 23:17:04 +0800

    增加一行log，测试提交情况

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("eden","hi,this is my test project!");
+        Log.i("eden","hi,this is my test projects!");
     }
 }
