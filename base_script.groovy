00a407fa6fddedabfc033d53d20a2ac3260ccca9
Eden <276354355@qq.com>
2021-12-26 17:52:41 +0800

    add base script

---
+++ base_script.groovy
@@ -0,0 +1,16 @@
+def checkout() {
+  println("base checkout")
+}
+
+def build() {
+  println("base build")
+}
+
+def commit() {
+  println("base commit")
+}
+
+def exportChangelog() {
+  println("base exportChangelog")
+}
+return this
--- jenkins_script.groovy
+++ jenkins_script.groovy
@@ -1,8 +1,13 @@
 def runUnitTest() {
+  baseSricpt.checkout()
+  baseSricpt.build()
+
   println("invoke runUnitTest")
 }
 
 def exportReporter() {
   println("invoke exportReporter")
+  baseSricpt.commit()
+  baseSricpt.exportChangelog()
 }
 return this
