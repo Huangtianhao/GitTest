8d0aed5eabb577d3d7e28ca78cd43faed91c3205
Eden <276354355@qq.com>
2021-12-26 18:24:27 +0800

    33

--- jenkins_script.groovy
+++ jenkins_script.groovy
@@ -1,8 +1,11 @@
+def baseSricpt
+
 def jenkins_script() {
   println("create jenkins_script")
 }
 
 def runUnitTest() {
+  initBase()
   baseSricpt.checkout()
   baseSricpt.build()
 
@@ -10,8 +13,14 @@ def runUnitTest() {
 }
 
 def exportReporter() {
+  initBase()
   println("invoke exportReporter")
   baseSricpt.commit()
   baseSricpt.exportChangelog()
+}
+def initBase() {
+  if(baseSricpt == null) {
+    def baseSricpt = fileLoader.fromGit('base_script.groovy', 'git@github.com:Huangtianhao/GitTest.git', 'master', '047190fb-4a69-4e2d-a6b7-2f56cb63ba2c', null)
+  }
 }
 return this
